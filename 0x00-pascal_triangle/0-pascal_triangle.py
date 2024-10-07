535f6623838a0624511a0b77ee72c0eaf08ac707
flowsar <brahim.sar151@gmail.com>
2024-10-07 01:58:05 +0100

    improve code for clarity

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-
 """Pascal triangle"""
 
 
 def pascal_triangle(n):
-    """Triangle Pasc"""
+    """Generate Pascal's Triangle with n rows."""
     if n <= 0:
         return []
-    tri = [[1]]
+
+    triangle = [[1]]
+
     for row_number in range(1, n):
         row = [1]
         for j in range(1, row_number):
-            element = tri[row_number - 1][j - 1] + tri[row_number - 1][j]
+            element = triangle[row_number - 1][j - 1]
+            element += triangle[row_number - 1][j]
             row.append(element)
         row.append(1)
-        tri.append(row)
+        triangle.append(row)
 
-    return tri
+    return triangle

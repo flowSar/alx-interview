536c91b206d83b259c5b3b927f75e10e16e3660a
flowsar <brahim.sar151@gmail.com>
2024-11-21 23:29:18 +0100

    rotate 2D matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""module with a function to rotate matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """rotate 2D matrix"""
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for row in matrix:
+        row.reverse()
